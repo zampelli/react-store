e533427f1618d3edfcb49ca6f8d74f0169c51d10
zampelli <milano121293@gmail.com>
2020-06-22 11:57:32 -0700

    base react store with routing

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./page/homepage/homepage.component";
+
+import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
